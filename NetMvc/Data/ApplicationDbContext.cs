1a7be392d62902e377258c6bb380f6a6ea67b525
nguyenthephong12g@gmail.com <nguyenthephong12g@gmail.com>
2024-09-17 16:08:52 +0700

    commit note

---
+++ NetMvc/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using NetMvc.Models;
+
+namespace NetMvc
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext (DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<NetMvc.Models.Student> Student { get; set; } = default!;
+    }
+}
