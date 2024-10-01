37e9f21337dafe0590ddc327284da4549b2c84e0
nguyenthephong12g@gmail.com <nguyenthephong12g@gmail.com>
2024-10-01 12:25:58 +0700

    Mô tả về các thay đổi

--- obj/Debug/net8.0/NetMvc.AssemblyInfo.cs
+++ obj/Debug/net8.0/NetMvc.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("NetMvc")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5f4739a8ad0efc3bc095f6a1c1f7a2571477ebae")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c74b9f1acb04fd4c3898df7df2945924a0791e64")]
 [assembly: System.Reflection.AssemblyProductAttribute("NetMvc")]
 [assembly: System.Reflection.AssemblyTitleAttribute("NetMvc")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
